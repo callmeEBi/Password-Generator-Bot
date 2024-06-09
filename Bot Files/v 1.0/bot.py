7b00c577e34f2d4d00b04242619e004e63d90955
callmeEBi <152092732+callmeEBi@users.noreply.github.com>
2024-06-09 12:54:05 -0400

    Create bot.py
    First version

---
+++ Bot Files/v 1.0/bot.py
@@ -0,0 +1,276 @@
+import logging
+from decouple import config
+from random import randrange
+from telegram import (
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    KeyboardButton,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+)
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    CallbackQueryHandler,
+    MessageHandler,
+    filters,
+)
+
+#! log
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+token = config("TOKEN")
+num_check, alpha_check, sym_check = [list("✅"), list("✅"), list("✅")]
+length = [15]
+
+history = {}
+
+
+def password_generator():
+    numbers = [i for i in range(10)]
+    alphbets = [chr(i) for i in range(65, 91)]
+    alphbets2 = [chr(i) for i in range(97, 123)]
+    alphbets.extend(alphbets2)
+    symbols = [
+        "!",
+        "@",
+        "#",
+        "$",
+        "&",
+        "*",
+        "+",
+        "-",
+        "_",
+    ]
+    selection_source = []
+    password = ""
+    if num_check[0] == "✅":
+        selection_source.extend(numbers)
+    if alpha_check[0] == "✅":
+        selection_source.extend(alphbets)
+    if sym_check[0] == "✅":
+        selection_source.extend(symbols)
+    if not selection_source:
+        return "Please select at least one character type."
+    i = 0
+    while i < length[0]:
+        j = randrange(len(selection_source))
+        password += str(selection_source[j])
+        i += 1
+    return password
+
+
+def status_changer(chk):
+    if chk[0] == "✅":
+        chk[0] = "❌"
+    else:
+        chk[0] = "✅"
+
+
+async def start(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE,
+    query=None,
+    not_first_time=False,
+):
+    start_buttons = [
+        [
+            InlineKeyboardButton("🔐 Generate Password", callback_data="gen"),
+            InlineKeyboardButton("⚙️ Settings", callback_data="settings"),
+        ],
+        [
+            InlineKeyboardButton("🗒 History", callback_data="history"),
+            InlineKeyboardButton("👋 Close", callback_data="close"),
+        ],
+    ]
+    start_markup = InlineKeyboardMarkup(start_buttons)
+    start_text = """
+Welcome to the password generator bot!
+
+This bot can generate strong and unique passwords for you.
+
+you can start working with bot through buttons below.
+"""
+    if not_first_time:
+        await query.edit_message_text(text=start_text, reply_markup=start_markup)
+    else:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            reply_to_message_id=update.effective_message.id,
+            text=start_text,
+            reply_markup=start_markup,
+        )
+
+
+async def length_function(update: Update, context: ContextTypes):
+    args = context.args[0]
+    try:
+        if 5 <= int(args) <= 40:
+            length[0] = args
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="Done.✅",
+                reply_to_message_id=update.effective_message.id,
+            )
+            return
+    except:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="""
+        ❌ The length must be a number bigger than 5 and smaller than 40 characters.
+    """,
+            reply_to_message_id=update.effective_message.id,
+        )
+
+
+async def handle_query(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    #! texts:
+    settings_text = """
+    Here are the settings:
+for changing password length you can use /len command.
+
+example : /len 10
+"""
+    #! buttons:
+    gen_buttons = [[InlineKeyboardButton("↩️ Back", callback_data="back")]]
+    settings_buttons = [
+        [
+            InlineKeyboardButton(f"⛓ Length : {length[0]}", callback_data="none"),
+            InlineKeyboardButton(f" Numbers : {num_check[0]}", callback_data="numbers"),
+        ],
+        [
+            InlineKeyboardButton(
+                f" Alphabets : {alpha_check[0]}", callback_data="alphabets"
+            ),
+            InlineKeyboardButton(f" Symbols : {sym_check[0]}", callback_data="symbols"),
+        ],
+        [InlineKeyboardButton("↩️ Back", callback_data="back")],
+    ]
+    history_buttons = [[InlineKeyboardButton("↩️ Back", callback_data="back")]]
+    #! markups
+    gen_markup = InlineKeyboardMarkup(gen_buttons)
+    settings_markup = InlineKeyboardMarkup(settings_buttons)
+    history_markup = InlineKeyboardMarkup(history_buttons)
+    query = update.callback_query
+    await query.answer()
+    qd = query.data
+    userID = update.effective_user.id
+    if qd == "gen":
+        password = password_generator()
+        if password.startswith("Please"):
+            await query.message.edit_text(
+                text=password,
+                reply_markup=gen_markup,
+            )
+            return
+
+        if userID not in history:
+            history[userID] = []
+        history[userID].append(password)
+        await query.message.edit_text(
+            text=f"Generated Password: <pre>{password}</pre>",
+            parse_mode="HTML",
+            reply_markup=gen_markup,
+        )
+    elif qd == "settings":
+        await query.message.edit_text(text=settings_text, reply_markup=settings_markup)
+    elif qd == "close":
+        await context.bot.delete_message(
+            chat_id=update.effective_chat.id, message_id=update.effective_message.id
+        )
+    elif qd == "history":
+        if userID not in history or not history[userID]:
+            await query.edit_message_text(
+                text="❗️ you don't have any generated passwords yet.",
+                reply_markup=history_markup,
+            )
+            return
+        temp_history = history[userID]
+        temp_history = [
+            f"{index+1} - <pre>{value}</pre>"
+            for index, value in enumerate(temp_history)
+        ]
+        temp_history = "\n\n".join(temp_history)
+        await query.edit_message_text(
+            text=f"your last generated passwords are here:\n\n{temp_history}",
+            reply_markup=history_markup,
+            parse_mode="HTML",
+        )
+    elif qd == "numbers":
+        status_changer(num_check)
+        settings_buttons = [
+            [
+                InlineKeyboardButton(f"⛓ Length : {length[0]}", callback_data="none"),
+                InlineKeyboardButton(
+                    f" Numbers : {num_check[0]}", callback_data="numbers"
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    f" Alphabets : {alpha_check[0]}", callback_data="alphabets"
+                ),
+                InlineKeyboardButton(
+                    f" Symbols : {sym_check[0]}", callback_data="symbols"
+                ),
+            ],
+            [InlineKeyboardButton("↩️ Back", callback_data="back")],
+        ]
+        settings_markup = InlineKeyboardMarkup(settings_buttons)
+        await query.message.edit_text(text=settings_text, reply_markup=settings_markup)
+    elif qd == "alphabets":
+        status_changer(alpha_check)
+        settings_buttons = [
+            [
+                InlineKeyboardButton(f"⛓ Length : {length[0]}", callback_data="none"),
+                InlineKeyboardButton(
+                    f" Numbers : {num_check[0]}", callback_data="numbers"
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    f" Alphabets : {alpha_check[0]}", callback_data="alphabets"
+                ),
+                InlineKeyboardButton(
+                    f" Symbols : {sym_check[0]}", callback_data="symbols"
+                ),
+            ],
+            [InlineKeyboardButton("↩️ Back", callback_data="back")],
+        ]
+        settings_markup = InlineKeyboardMarkup(settings_buttons)
+        await query.message.edit_text(text=settings_text, reply_markup=settings_markup)
+    elif qd == "symbols":
+        status_changer(sym_check)
+        settings_buttons = [
+            [
+                InlineKeyboardButton(f"⛓ Length : {length[0]}", callback_data="none"),
+                InlineKeyboardButton(
+                    f" Numbers : {num_check[0]}", callback_data="numbers"
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    f" Alphabets : {alpha_check[0]}", callback_data="alphabets"
+                ),
+                InlineKeyboardButton(
+                    f" Symbols : {sym_check[0]}", callback_data="symbols"
+                ),
+            ],
+            [InlineKeyboardButton("↩️ Back", callback_data="back")],
+        ]
+        settings_markup = InlineKeyboardMarkup(settings_buttons)
+        await query.message.edit_text(text=settings_text, reply_markup=settings_markup)
+    elif qd == "back":
+        await start(update, context, query=query, not_first_time=True)
+
+
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(token).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("len", length_function))
+    app.add_handler(CallbackQueryHandler(handle_query))
+    app.run_polling()
